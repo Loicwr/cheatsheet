b4f4c4f0bda0b94f0bf6055a63e9c258d4acbda3
Loicwr <nephtyse19@hotmail.fr>
2025-05-26 14:59:41 +0200

    fix: comment out animal class and its instances for clarity

--- Js devrfront/test.js
+++ Js devrfront/test.js
@@ -1,15 +1,15 @@
-class animal {
-    constructor(nom, espece){
-        this.nom = nom;
-        this.espece = espece;
-    }
-    presentation(){
-        console.log("Je suis " + this.nom + ", un " + this.espece + ".")
-    }
-}
+// class animal {
+//     constructor(nom, espece){
+//         this.nom = nom;
+//         this.espece = espece;
+//     }
+//     presentation(){
+//         .console.log("Je suis " + this.nom + ", un " + this.espece + ".")
+//     }
+// }
 
-let a1 = new animal("rex", "chien");
-a1.presentation();
+// let a1 = new animal("rex", "chien");
+// a1.presentation();
 
-let a2 = new animal("Miaou", "chat")
-a2.presentation();
+// let a2 = new animal("Miaou", "chat")
+// a2.presentation();
