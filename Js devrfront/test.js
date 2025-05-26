1609ee10f17063275b6398bda6cbab0c8efa143b
Loicwr <nephtyse19@hotmail.fr>
2025-05-26 15:37:30 +0200

    fix: comment out unused animal and product classes for clarity

--- Js devrfront/test.js
+++ Js devrfront/test.js
@@ -13,3 +13,18 @@
 
 // let a2 = new animal("Miaou", "chat")
 // a2.presentation();
+
+// class Produit{
+//     constructor(nom, prix, stock){
+//         this.nom = nom;
+//         this.prix = prix;
+//         this.stock = stock;
+//     }
+//     afficherInfos(){
+//         console.log("Produit :" + this.nom + ", Prix : " + this.prix + "euros, " + "Stock : " + this.stock);
+//     OU  console.log(`Produit : ${this.nom} - Prix : ${this.prix}€ - Stock : ${this.stock} unités`);
+//     }
+// }
+
+// let p1 = new Produit ("banane", 15, 62)
+// p1.afficherInfos();
